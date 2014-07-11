a5ab66ba33c714d40eb1ec2497b3afa71a82366a
daniele.giardini@gmail.com <daniele.giardini@gmail.com@7c4ab8a0-76a6-45ed-d0f5-36f15d7fc04d>
2014-07-11 15:57:48 +0000

    - All creation chains now work and return a Tween - Added IsBackwards() to Tween

---
+++ tags/0.1.550/DOTween/Extensions.cs
@@ -0,0 +1,114 @@
+﻿// Author: Daniele Giardini - http://www.demigiant.com
+// Created: 2014/07/05 18:31
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// 
+
+using DG.Tween.Core;
+
+namespace DG.Tween
+{
+    public static class Extensions
+    {
+        // ===================================================================================
+        // TWEENER + SEQUENCES ---------------------------------------------------------------
+
+        // Play operations
+
+        public static void Complete(this Tween t)
+        {
+            TweenManager.Complete(t);
+        }
+        
+        public static void Flip(this Tween t)
+        {
+            TweenManager.Flip(t);
+        }
+
+        public static void Goto(this Tween t, float to, bool andPlay = false)
+        {
+            TweenManager.Goto(t, to, andPlay);
+        }
+
+        public static void Kill(this Tween t)
+        {
+            if (TweenManager.IsActive(t)) {
+                if (TweenManager.isUpdateLoop && TweenManager.updateLoopType == t.updateType) {
+                    // Just mark it for killing, so the update loop will take care of it
+                    t.active = false;
+                } else TweenManager.Despawn(t);
+            }
+        }
+
+        public static Tween Pause(this Tween t)
+        {
+            TweenManager.Pause(t);
+            return t;
+        }
+
+        public static Tween Play(this Tween t)
+        {
+            TweenManager.Play(t);
+            return t;
+        }
+
+        public static void PlayBackwards(this Tween t)
+        {
+            TweenManager.PlayBackwards(t);
+        }
+
+        public static void PlayForward(this Tween t)
+        {
+            TweenManager.PlayForward(t);
+        }
+
+        public static void Restart(this Tween t, bool includeDelay = true)
+        {
+            TweenManager.Restart(t, includeDelay);
+        }
+
+        public static void Rewind(this Tween t, bool includeDelay = true)
+        {
+            TweenManager.Rewind(t, includeDelay);
+        }
+
+        // Info getters
+
+        public static int CompletedLoops(this Tween t)
+        {
+            return t.completedLoops;
+        }
+
+        public static float Position(this Tween t)
+        {
+            return t.position;
+        }
+
+        public static float Elapsed(this Tween t)
+        {
+            int loopsToCount = t.position >= t.duration ? t.completedLoops - 1 : t.completedLoops;
+            return (loopsToCount * t.duration) + t.position;
+        }
+
+        public static bool IsBackwards(this Tween t)
+        {
+            return t.isBackwards;
+        }
+    }
+}
---
+++ tags/0.1.550/DOTween/Plugins/PlugVector3X.cs
@@ -0,0 +1,97 @@
+﻿// Author: Daniele Giardini - http://www.demigiant.com
+// Created: 2014/07/07 14:33
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// 
+
+using DG.Tween.Core;
+using DG.Tween.Plugins.Core;
+using UnityEngine;
+
+namespace DG.Tween.Plugins
+{
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // ||| PLUG ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    public struct PlugVector3X : IPlugSetter<Vector3, float, Vector3XPlugin, PlugVector3X.Options>
+    {
+        readonly float _endValue;
+        readonly MemberGetter<Vector3> _getter;
+        readonly MemberSetter<Vector3> _setter;
+        readonly Options _options;
+
+        public PlugVector3X(MemberGetter<Vector3> getter, MemberSetter<Vector3> setter, float endValue, Options options = new Options())
+        {
+            _getter = getter;
+            _setter = setter;
+            _endValue = endValue;
+            _options = options;
+        }
+
+        public MemberGetter<Vector3> Getter() { return _getter; }
+        public MemberSetter<Vector3> Setter() { return _setter; }
+        public float EndValue() { return _endValue; }
+        public Options GetOptions() { return _options; }
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        // ||| OPTIONS |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        public struct Options
+        {
+            public bool snapping;
+
+            public Options(bool snapping)
+            {
+                this.snapping = snapping;
+            }
+        }
+    }
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // ||| PLUGIN ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    public class Vector3XPlugin : ABSTweenPlugin<Vector3, float, PlugVector3X.Options>
+    {
+        public override float ConvertT1toT2(PlugVector3X.Options options, Vector3 value)
+        {
+            return value.x;
+        }
+
+        public override float GetRelativeEndValue(PlugVector3X.Options options, float startValue, float changeValue)
+        {
+            return startValue + changeValue;
+        }
+
+        public override float GetChangeValue(PlugVector3X.Options options, float startValue, float endValue)
+        {
+            return endValue - startValue;
+        }
+
+        public override Vector3 Evaluate(PlugVector3X.Options options, bool isRelative, MemberGetter<Vector3> getter, float elapsed, float startValue, float changeValue, float duration, EaseFunction ease)
+        {
+            Vector3 res = getter();
+            res.x = ease(elapsed, startValue, changeValue, duration, 0, 0);
+            if (options.snapping) res.x = Mathf.Round(res.x);
+            return res;
+        }
+    }
+}
