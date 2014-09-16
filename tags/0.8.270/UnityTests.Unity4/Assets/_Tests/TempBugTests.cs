c3fb02190ca42b34ef3d690686a6cd733de44b5b
daniele.giardini@gmail.com <daniele.giardini@gmail.com@7c4ab8a0-76a6-45ed-d0f5-36f15d7fc04d>
2014-09-16 15:15:35 +0000

    Before changing From logic

---
+++ tags/0.8.270/UnityTests.Unity4/Assets/_Tests/TempBugTests.cs
@@ -0,0 +1,36 @@
+﻿using DG.Tweening;
+using System.Collections;
+using UnityEngine;
+
+public class TempBugTests : BrainBase
+{
+	public GameObject[] targets;
+
+	public int AnimTarget = 0;
+ 
+    void Start ( ) {
+        DOTween.Init ( true, true, LogBehaviour.Verbose );
+    }
+ 
+    void OnGUI ( ) {
+        if (GUILayout.Button ( "Next" )) {
+            NextButtonPressed ( );
+        }
+    }
+ 
+    public void Animate ( ) {
+        if (AnimTarget == 0) {
+            targets[0].renderer.material.DOFade ( 0f, 3f ).OnComplete( NextButtonPressed );
+        } else if (AnimTarget == 1) {
+            targets[1].renderer.material.DOFade ( 0f, 3f ).OnComplete ( NextButtonPressed );
+        } else if (AnimTarget == 2) {
+            targets[2].renderer.material.DOFade ( 0f, 3f ).OnComplete ( NextButtonPressed );
+        }
+    }
+ 
+    public void NextButtonPressed ( ) {
+        DOTween.Complete ( );
+        Animate ( );
+        AnimTarget++;
+    }
+}
