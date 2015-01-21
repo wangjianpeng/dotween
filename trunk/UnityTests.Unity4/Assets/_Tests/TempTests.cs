76d05a8c536d8f17c23309a4b6a46bbd6db9089f
daniele.giardini@gmail.com <daniele.giardini@gmail.com@7c4ab8a0-76a6-45ed-d0f5-36f15d7fc04d>
2015-01-21 10:20:35 +0000

    Fixed safe mode not working when target was destroyed before a special startup

--- trunk/UnityTests.Unity4/Assets/_Tests/TempTests.cs
+++ trunk/UnityTests.Unity4/Assets/_Tests/TempTests.cs
@@ -5,45 +5,17 @@ using System;
  
 public class TempTests : BrainBase
 {
-    public float test1 = 0f;
-    public float test2 = 0f;
+    public Transform[] targets;
  
-    bool running = false;
- 
-    void Update()
+    void Start()
     {
-        if(Input.GetKeyDown("space"))
-        {
-            Test();
-        }
+        targets[0].DOMoveX(3, 1).SetLoops(-1).SetAutoKill(false).OnKill(()=> Debug.Log("Tween Killed"));
+
+        DOTween.Sequence().Append(targets[1].DOMoveX(3, 1)).SetLoops(-1).SetAutoKill(false).OnKill(()=> Debug.Log("Sequence Killed"));
     }
- 
-    void Test()
-    {
-        running = true;
- 
-        DOTween.To(() => test1, x => test1 = x, 1f, 1f)
-            .SetEase(Ease.OutSine)
-            .SetUpdate(UpdateType.Late, true)
-            .OnComplete(() => test1 = 0f);
- 
-        // BUG:  this tween will stick if set to UpdateType.Default and not UpdateType.Late
-        // mixing-and-matching UpdateType will cause bugs, but not one or the other
- 
-        DOTween.To(() => test2, x => test2 = x, 1f, 1f)
-            .SetEase(Ease.OutSine)
-            .SetUpdate(UpdateType.Normal, true)
-            .SetDelay(0.25f) // only shows up if a delay is set here
-            .OnComplete(() => test2 = 0f);
-    }
- 
+
     void OnGUI()
     {
-        GUILayout.BeginVertical("box");
-        GUILayout.Label("Running: " + running);
-        GUILayout.Label("Test1: " + String.Format("{0:0.00}", test1));
-        GUILayout.Label("Test2: " + String.Format("{0:0.00}", test2));
-        GUILayout.Label("Time: " + String.Format("{0:0.00}", Time.realtimeSinceStartup));
-        GUILayout.EndVertical();
+        if (GUILayout.Button("KILL")) DOTween.Kill();
     }
 }
