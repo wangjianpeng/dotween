7df18807b3573f9f40215a6483a388580d16812c
daniele.giardini@gmail.com <daniele.giardini@gmail.com@7c4ab8a0-76a6-45ed-d0f5-36f15d7fc04d>
2015-01-30 11:28:18 +0000

    Implemented regular callbacks like OnComplete with empty Sequences

--- trunk/DOTween/DOTween.cs
+++ trunk/DOTween/DOTween.cs
@@ -21,7 +21,7 @@ namespace DG.Tweening
     public class DOTween
     {
         /// <summary>DOTween's version</summary>
-        public static readonly string Version = "1.0.115";
+        public static readonly string Version = "1.0.120";
 
         ///////////////////////////////////////////////
         // Options ////////////////////////////////////
