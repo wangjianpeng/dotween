9213ac38653ae122583f52cb22426956674a93b8
daniele.giardini@gmail.com <daniele.giardini@gmail.com@7c4ab8a0-76a6-45ed-d0f5-36f15d7fc04d>
2015-02-23 18:06:27 +0000

    TweenCallback and EaseFunction moved to DG.Tweening namespace

--- DOTween/Core/Delegates.cs
+++ DOTween/Core/Delegates.cs
@@ -4,18 +4,8 @@
 // License Copyright (c) Daniele Giardini.
 // This work is subject to the terms at http://dotween.demigiant.com/license.php
 
-namespace DG.Tweening.Core
+namespace DG.Tweening
 {
-    /// <summary>
-    /// Used in place of <c>System.Func</c>, which is not available in mscorlib.
-    /// </summary>
-    public delegate T DOGetter<out T>();
-
-    /// <summary>
-    /// Used in place of <c>System.Action</c>.
-    /// </summary>
-    public delegate void DOSetter<in T>(T pNewValue);
-
     /// <summary>
     /// Used for tween callbacks
     /// </summary>
@@ -29,4 +19,17 @@ namespace DG.Tweening.Core
     /// Used for custom and animationCurve-based ease functions. Must return a value between 0 and 1.
     /// </summary>
     public delegate float EaseFunction(float time, float duration, float overshootOrAmplitude, float period);
+}
+
+namespace DG.Tweening.Core
+{
+    /// <summary>
+    /// Used in place of <c>System.Func</c>, which is not available in mscorlib.
+    /// </summary>
+    public delegate T DOGetter<out T>();
+
+    /// <summary>
+    /// Used in place of <c>System.Action</c>.
+    /// </summary>
+    public delegate void DOSetter<in T>(T pNewValue);
 }
