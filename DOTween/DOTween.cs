84e05e29e10f5aaaf8159e9dbb974e703f35899b
daniele.giardini@gmail.com <daniele.giardini@gmail.com@7c4ab8a0-76a6-45ed-d0f5-36f15d7fc04d>
2015-03-13 20:52:11 +0000

    Fixed GetTweensById/Target returning a NullReferenceException in some cases

--- DOTween/DOTween.cs
+++ DOTween/DOTween.cs
@@ -21,7 +21,7 @@ namespace DG.Tweening
     public class DOTween
     {
         /// <summary>DOTween's version</summary>
-        public static readonly string Version = "1.0.305";
+        public static readonly string Version = "1.0.306";
 
         ///////////////////////////////////////////////
         // Options ////////////////////////////////////
